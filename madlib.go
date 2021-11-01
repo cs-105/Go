3da5a2012d769aeb752e02e6e188934476af3265
twbjelland <60273828+twbjelland@users.noreply.github.com>
2021-11-01 15:13:35 -0700

    pseudocode main function

--- madlib.go
+++ madlib.go
@@ -1,15 +1,29 @@
 //Main function should output a welcome message to the user and then get user input for a topic. 
 //Main should call the findText function to search online for a body of text relating to the specified topic.
+//main should then, for every word removed, request and retrive a word from the user of equivalent type, which are all checked by inputValue
+//main then prints out the now edited body of text and asks the user if they would like to do another or quit
+
+//global variable text
+
+/*
+main
+  output welcome message to user
+  get user input for a topic
+  pass user input to findText function
+  call textParser on text to get words to replace
+  for each word to replace, output part of speech to user and call inputvalidation
+  when all words have been replace, replace words from og text with user inputted words
+  print out body of text
+  ask user if they want to repeat or quit
+*/
+
 
 //findText function should pass the topic in string form and return a relevant body of text found on the internet. This text is then parsed by textParser
 
 //textParser should return the location and type of words that are being replaced
 
-//main should then, for every word removed, request and retrive a word from the user of equivalent type, which are all checked by inputValue
-
 //inputValue should prevent users from inputting a word of different type than the word that was removed
 
 //insertWords should take the validated user inputs and pass them back into the body of text
 
-//main then prints out the now edited body of text and asks the user if they would like to do another or quit
 
