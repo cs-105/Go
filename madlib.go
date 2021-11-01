ace0652fbe0b22c9bcd6fc4eabaff0ea3999065a
mtoddwestmont <90293087+mtoddwestmont@users.noreply.github.com>
2021-11-01 15:06:38 -0700

    add roadmap

---
+++ madlib.go
@@ -0,0 +1,15 @@
+//Main function should output a welcome message to the user and then get user input for a topic. 
+//Main should call the findText function to search online for a body of text relating to the specified topic.
+
+//findText function should pass the topic in string form and return a relevant body of text found on the internet. This text is then parsed by textParser
+
+//textParser should return the location and type of words that are being replaced
+
+//main should then, for every word removed, request and retrive a word from the user of equivalent type, which are all checked by inputValue
+
+//inputValue should prevent users from inputting a word of different type than the word that was removed
+
+//insertWords should take the validated user inputs and pass them back into the body of text
+
+//main then prints out the now edited body of text and asks the user if they would like to do another or quit
+
