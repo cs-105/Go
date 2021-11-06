ded42183e48fb1cb99aeae4e00f7ef506e2fbe5f
ethanbanez <90736644+ethanbanez@users.noreply.github.com>
2021-11-05 22:15:01 -0700

    I hope this works

--- GoogleScraper.go
+++ GoogleScraper.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	g "github.com/serpapi/google-search-results-golang"
 )
 
-func results() {
+func main() {
 	api := "8a54bed3f33a5d9127170bc6b3af978878ba7400e9e4c1cf3e0476fdada43320"
 	parameters := map[string]string{
 		"engine":  "google",
@@ -13,6 +15,11 @@ func results() {
 	}
 
 	query := g.NewGoogleSearch(parameters, api)
-	results, err := search.GetJSON()
-	organic_results := results["organic_results"].([]interface{})
+	results, err := query.GetJSON()
+	if err != nil {
+		fmt.Println(err)
+	}
+	organicresults := results["organic_results"].([]interface{})
+	first_result := organicresults[0].(map[string]interface{})
+	fmt.Println(first_result["title"].(string))
 }
