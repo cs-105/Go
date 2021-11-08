d68ac36082bcd9d9d7219f1a565d5563271e7566
ethanbanez <90736644+ethanbanez@users.noreply.github.com>
2021-11-08 13:12:52 -0800

    scraper.go

---
+++ scraper.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	g "github.com/serpapi/google-search-results-golang"
+)
+
+func Scrape() {
+	api := "8a54bed3f33a5d9127170bc6b3af978878ba7400e9e4c1cf3e0476fdada43320"
+
+	fmt.Println("What kind of articles would you like to madlib on?")
+	reader := bufio.NewReader(os.Stdin)
+	searchTerm, _ := reader.ReadString('\n')
+
+	parameters := map[string]string{
+		"engine":  "google",
+		"q":       searchTerm,
+		"output":  "html",
+		"api_key": "8a54bed3f33a5d9127170bc6b3af978878ba7400e9e4c1cf3e0476fdada43320",
+	}
+
+	query := g.NewGoogleSearch(parameters, api)
+	results, err := query.GetJSON()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	organicresults := results["organic_results"].([]interface{})
+	page := organicresults[1].(map[string]interface{})
+	url := fmt.Sprintf("%v", page["link"])
+	ReadHTML(url)
+	/* var pageReturned map[string]interface{}
+	for _, v := range organicresults {
+		pageReturned = v.(map[string]interface{})
+		fmt.Printf("%v: %v\n", pageReturned["title"], pageReturned["link"])
+	} */
+}
