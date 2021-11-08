dc24fdbd4ca65a97828b4a441f6effb5516558f0
ethanbanez <90736644+ethanbanez@users.noreply.github.com>
2021-11-08 01:37:08 -0800

    Updated googleScraper and readHTML

--- GoogleScraper.go
+++ GoogleScraper.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	g "github.com/serpapi/google-search-results-golang"
 )
 
 func main() {
 	api := "8a54bed3f33a5d9127170bc6b3af978878ba7400e9e4c1cf3e0476fdada43320"
+
+	fmt.Println("What kind of articles would you like to madlib on?")
+	reader := bufio.NewReader(os.Stdin)
+	searchTerm, _ := reader.ReadString('\n')
+
 	parameters := map[string]string{
 		"engine":  "google",
-		"q":       "dog",
+		"q":       searchTerm,
+		"output":  "html",
 		"api_key": "8a54bed3f33a5d9127170bc6b3af978878ba7400e9e4c1cf3e0476fdada43320",
 	}
 
@@ -19,7 +27,11 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	organicresults := results["organic_results"].([]interface{})
-	first_result := organicresults[0].(map[string]interface{})
-	fmt.Println(first_result["title"].(string))
+	var pageReturned map[string]interface{}
+	for _, v := range organicresults {
+		pageReturned = v.(map[string]interface{})
+		fmt.Printf("%v: %v\n", pageReturned["title"], pageReturned["link"])
+	}
 }
