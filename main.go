6865c3a5cb7c2839afd0d536da33196b76abe13b
mtoddwestmont <mtodd@westmont.edu>
2021-11-17 17:28:50 -0800

    Collect user input to replace tagged words

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
 )
 
 //global variables
@@ -45,15 +43,15 @@ func main() {
 	text = "This is how the birth of Jesus the Messiah came about: His mother Mary was pledged to be married to Joseph, but before they came together, she was found to be pregnant through the Holy Spirit. Because Joseph her husband was faithful to the law, and yet did not want to expose her to public disgrace, he had in mind to divorce her quietly. But after he had considered this, an angel of the Lord appeared to him in a dream and said, “Joseph son of David, do not be afraid to take Mary home as your wife, because what is conceived in her is from the Holy Spirit. She will give birth to a son, and you are to give him the name Jesus, because he will save his people from their sins.” All this took place to fulfill what the Lord had said through the prophet: “The virgin will conceive and give birth to a son, and they will call him Immanuel” (which means “God with us”). When Joseph woke up, he did what the angel of the Lord had commanded him and took Mary home as his wife. But he did not consummate their marriage until she gave birth to a son. And he gave him the name Jesus."
 	parseText()
 
-	fmt.Println(wordsToReplace)
-
-	insertWords()
-
-	cmd := exec.Command("espeak", text)
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+	for _, element := range posToReplace {
+		fmt.Println("Please enter the following word type: ", element)
+		var newWord string
+		fmt.Scanln(&newWord)
+		fmt.Println("You have entered: ", newWord)
+		userInputWords = append(userInputWords, newWord)
 	}
 
+	insertWords()
 }
 
 //findText function should pass the topic in string form and return a relevant body of text found on the internet. This text is then parsed by textParser
