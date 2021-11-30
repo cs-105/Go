cbcfe8c9877d568dc25a060c3635685ccd497f54
ethanbanez <90736644+ethanbanez@users.noreply.github.com>
2021-11-29 16:42:55 -0800

    scraper.go now returns a string

--- scraper.go
+++ scraper.go
@@ -6,16 +6,16 @@ import (
 
 var api = "8a54bed3f33a5d9127170bc6b3af978878ba7400e9e4c1cf3e0476fdada43320"
 
-func Scrape(topic, searchTerm string) {
+func Scrape(topic, searchTerm string) string {
 	topic = strings.ToLower(topic)
-
+	var result string
 	switch topic {
 	case "wiki":
-		WikiRetriever(searchTerm)
+		result = WikiRetriever(searchTerm)
 	case "news":
-		NewsRetriever(searchTerm)
+		result = NewsRetriever(searchTerm)
 	case "songs":
-		LyricRetriever(searchTerm)
+		result = LyricRetriever(searchTerm)
 	}
-
+	return result
 }
